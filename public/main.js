fe14f19c85a12fac353b4083d12cb3c204f9844f
shawn-chambers <chambers.shawn.89@gmail.com>
2021-08-24 08:34:29 -0700

    refined animation and added wiggle

--- public/main.js
+++ public/main.js
@@ -3,19 +3,19 @@ var tl = new TimelineMax({ onUpdate: updatePercentage });
 const controller = new ScrollMagic.Controller();
 
 tl
-  .from('#eyes', .5, { y: -2000})
-  .from('#mustache', .5, { y: 2000})
-  .from('#left-ear', .5, { x: 2000})
-  .from('#right-ear', .5, { x: -2000})
-  .from('#shoes', .5, { y: 2000})
-  .from('#right-arm', .5, { y: -2000})
-  .from('#left-arm', .5, { x: 2000})
-  .from('#glasses', .5, { y: -2000})
+  .from('#eyes', 2, { y: -2000})
+  .from('#mustache', 2, { y: 2000})
+  .from('#left-ear', 2, { x: 2000})
+  .from('#right-ear', 2, { x: -2000}, "=-2")
+  .from('#shoes', 2, { y: 2000})
+  .from('#right-arm', 2, { y: -2000})
+  .from('#left-arm', 2, { x: 2000})
+  .from('#glasses', 2, { y: -2000})
   
 const scene = new ScrollMagic.Scene({
   triggerElement: '.sticky',
   triggerHook: 'onLeave',
-  duration: '100%'
+  duration: '200%'
 })
   .setPin('.sticky')
   .setTween(tl)
@@ -25,3 +25,7 @@ function updatePercentage() {
   tl.progress();
   console.log(tl.progress());
 }
+
+let mustache = document.getElementById('mustache');
+console.log(mustache);
+mustache.addEventListener('mouseenter', (e) => mustache.classList.add('hover'));
